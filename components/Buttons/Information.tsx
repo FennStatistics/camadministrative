4137e0c907d1cb8703f5b4d387f894a978ac8249
Camel-app <camel.app@proton.me>
2024-06-11 11:17:43 +0200

    first page responsive

--- components/Buttons/Information.tsx
+++ components/Buttons/Information.tsx
@@ -4,7 +4,7 @@ export default function ButtonInformation() {
   return(
     <Link
     href="/information"
-    className="py-2 px-3 flex rounded-md bg-gray-200 hover:bg-gray-500 text-xl"
+    className="py-2 px-3 flex rounded-md bg-gray-200 hover:bg-gray-500 md:text-xl sm:text-sm"
   >
     Information
   </Link>
--- components/Buttons/OnlineDocumentation.tsx
+++ components/Buttons/OnlineDocumentation.tsx
@@ -4,7 +4,7 @@ export default function ButtonOnlineDocumentation() {
   return(
     <Link
     href="https://camtools-documentation.readthedocs.io/en/master/"
-    className="py-2 px-3 flex rounded-md bg-gray-200 hover:bg-gray-500 text-xl"
+    className="py-2 px-3 flex rounded-md bg-gray-200 hover:bg-gray-500 md:text-xl sm:text-sm"
     target="_blank"
   >
     Online Documentation
